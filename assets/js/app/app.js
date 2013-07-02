bd6d29c1f8d9311f90da479b80164717c88dd27b
Xindong Ding <dxd.spirits@gmail.com>
2013-07-02 18:56:28 +0800

    Add basic http auth to backbone.

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -18,79 +18,38 @@ var MeiweiApp = new (Backbone.View.extend({
 
 	log: function(msg) { console.log(msg) },
     
+    bindBasicAuth: function() {
+		var token = null;
+		var encode = function(username, password) {
+			return btoa(username + ':' + password);
+		};
+		
+		var originalSync = Backbone.sync;
+		Backbone.sync = function(method, model, options) {
+			if (typeof token !== "undefined" && token !== null) {
+				options.headers = options.headers || {};
+				_.extend(options.headers, { 'Authorization': 'Basic ' + token });
+			}
+			return originalSync.call(model, method, model, options);
+		};
+		
+		Backbone.BasicAuth = {
+			set: function(username, password) {
+				token = encode(username, password);
+			},
+			clear: function() {
+				token = null;
+			}
+		};
+    },
+    
 	start: function() {
+		MeiweiApp.bindBasicAuth();
+		Backbone.BasicAuth.set('dxdeat', '123abc');
 		Backbone.history.start();
 	}
 }))({el: document.body});
 
-MeiweiApp.Collection = Backbone.Collection.extend({
-	parse: function(response) {
-		if (response.results != null) {
-			this.count = response.count;
-			this.previous = response.previous;
-			this.next = response.next;
-			return response.results;
-		} else {
-			return response;
-		}
-	}
-});
-
-MeiweiApp.Model = Backbone.Model.extend({
-	url: function() {
-		if (this.attributes.url) {
-			return this.attributes.url;
-		} else {
-			var origUrl = Backbone.Model.prototype.url.call(this);
-        	return origUrl + (origUrl.charAt(origUrl.length - 1) == '/' ? '' : '/');
-		}
-	}
-});
-
-MeiweiApp.View = Backbone.View.extend({});
-
-MeiweiApp.ModelView = Backbone.View.extend({
-	initialize: function() {
-		this.model.on('change', this.render, this);
-	},
-	template: Mustache.compile(""),
-	render: function() {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	}
-});
-
-MeiweiApp.CollectionView = Backbone.View.extend({
-	modelView: MeiweiApp.ModelView,
-	initialize: function() {
-		this.collection.on('reset', this.addAll, this);
-		this.collection.on('add', this.addOne, this);
-	},
-	addOne: function(item) {
-		this.$el.append((new this.modelView({model: item})).render().el);
-	},
-	addAll: function() {
-		this.$el.html("");
-		this.collection.forEach(this.addOne, this);
-	},
-	render: function() {
-	    this.addAll();
-	    return this;
-	}
-});
-
-MeiweiApp.PageView = Backbone.View.extend({
-	slideIn: function() {
-		if (this.$el && this.$el.hasClass('view-hidden')) {
-			$('.view').addClass('view-hidden');
-			this.$el.removeClass('view-hidden');
-		}
-	},
-	show: function() {
-		this.render();
-		this.slideIn();
-	}
-});
 
 $(function(){
 	MeiweiApp.start();
--- assets/js/app/views.js
+++ assets/js/app/views.js
@@ -1,4 +1,50 @@
 
+MeiweiApp.View = Backbone.View.extend({});
+
+MeiweiApp.ModelView = Backbone.View.extend({
+	initialize: function() {
+		this.model.on('change', this.render, this);
+	},
+	template: Mustache.compile(""),
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
+MeiweiApp.CollectionView = Backbone.View.extend({
+	modelView: MeiweiApp.ModelView,
+	initialize: function() {
+		this.collection.on('reset', this.addAll, this);
+		this.collection.on('add', this.addOne, this);
+	},
+	addOne: function(item) {
+		this.$el.append((new this.modelView({model: item})).render().el);
+	},
+	addAll: function() {
+		this.$el.html("");
+		this.collection.forEach(this.addOne, this);
+	},
+	render: function() {
+	    this.addAll();
+	    return this;
+	}
+});
+
+MeiweiApp.PageView = Backbone.View.extend({
+	slideIn: function() {
+		if (this.$el && this.$el.hasClass('view-hidden')) {
+			$('.view').addClass('view-hidden');
+			this.$el.removeClass('view-hidden');
+		}
+	},
+	show: function() {
+		this.render();
+		this.slideIn();
+	}
+});
+
+
 /********************************** Home **********************************/
 
 MeiweiApp.Views.RecommendListItem = MeiweiApp.ModelView.extend({
