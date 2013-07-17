edd506a1dfd536df80b51b9b37f2b98bc8036543
liah <liahchan1213@gmail.com>
2013-07-17 16:37:28 +0800

    Cleanup attending.js

--- assets/js/app/views/attending.js
+++ assets/js/app/views/attending.js
@@ -3,12 +3,6 @@
 MeiweiApp.Pages.Attending = new (MeiweiApp.PageView.extend({
 	initPage: function() {
 		this.orders = new MeiweiApp.Collections.Orders();
-        this.views = {
-        	orderList: new MeiweiApp.Views.OrderList1({
-	            collection: this.orders,
-	            el: this.$('.scroll .scroll-inner')
-	        })
-	    }
 	},
     render: function() {
         $.when(
