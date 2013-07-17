9c7a60f4fea30cb625537ccbac1a033b69d05036
liah <liahchan1213@gmail.com>
2013-07-17 16:20:00 +0800

    add attending page css.

--- assets/js/app/views/attending.js
+++ assets/js/app/views/attending.js
@@ -1,23 +1,12 @@
 
-MeiweiApp.Views.OrderList1 = MeiweiApp.CollectionView.extend({
-	ModelView: MeiweiApp.ModelView.extend({
-		template: MeiweiApp.Templates['order-list-item']
-	})
-});
 
 MeiweiApp.Pages.Attending = new (MeiweiApp.PageView.extend({
 	initPage: function() {
-		this.orders = new MeiweiApp.Collections.Orders();
-        this.views = {
-        	orderList: new MeiweiApp.Views.OrderList1({
-	            collection: this.orders,
-	            el: this.$('.scroll .wrapper')
-	        })
-	    }
+		//this.orders = new MeiweiApp.Collections.Orders();
 	},
     render: function() {
         $.when(
-        	this.orders.fetch({reset: true})
+        	//this.orders.fetch({reset: true})
         ).then(this.showPage);
     }
 }))({el: $("#view-attending")});
