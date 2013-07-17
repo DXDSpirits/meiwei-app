312d1726f9b5b233d98b3575e2d6e630e09f3af8
Xindong Ding <dxd.spirits@gmail.com>
2013-07-17 18:15:10 +0800

    Modify router names.

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -6,58 +6,37 @@ MeiweiApp.Router = new (Backbone.Router.extend({
 		this.route('', 'index');
 		this.route('home', 'home');
 		
-		this.route(/^restaurant\/(\d+)$/, 'restaurant');
+		this.route(/^restaurant\/(\d+)$/, 'restaurantDetail');
 		this.route(/^restaurant\/(\d+)\/order$/, 'restaurantOrder');
-		this.route('restaurant/search', 'RestaurantSearch');
+		this.route('restaurant/search', 'restaurantSearch');
 		
 		this.route('member', 'memberCenter');
 		this.route('member/login', 'memberLogin');
 		this.route('member/profile', 'memberProfile');
-		
-		this.route('member/order', 'memberOrders');
-		this.route(/^member\/order\/(\d+)$/, 'orderDetail');
 		this.route('member/contacts', 'memberContacts');
 		
-		this.route('member/attending', 'memberAttending');
-	},
-	index: function(){
-		MeiweiApp.Router.navigate('home', {trigger: true});
-	},
-	home: function(){
-		MeiweiApp.Pages.Home.go();
+		this.route('order', 'orderList');
+		this.route(/^order\/(\d+)$/, 'orderDetail');
+		
+		this.route('attending', 'attending');
 	},
 	
-	restaurant: function(rid) {
-		MeiweiApp.Pages.Restaurant.go(rid);
-	},
-	RestaurantSearch: function(){
-		MeiweiApp.Pages.RestaurantSearch.go();
-	},
-	restaurantOrder: function(rid){
-		MeiweiApp.Pages.RestaurantOrder.go(rid);
-	},
+	index: function() { MeiweiApp.Router.navigate('home', {trigger: true}); },
+	home: function() { MeiweiApp.Pages.Home.go(); },
 	
-	memberCenter: function(){
-		MeiweiApp.Pages.MemberCenter.go();
-	},
-	memberLogin: function(){
-		MeiweiApp.Pages.MemberLogin.go();
-	},
-	memberProfile: function(){
-		MeiweiApp.Pages.MemberProfile.go();
-	},
-	memberOrders: function(){
-        MeiweiApp.Pages.MemberOrders.go();
-    },
-    memberContacts: function(){
-        MeiweiApp.Pages.MemberContacts.go();
-    },
-    orderDetail: function(orderId) {
-    	MeiweiApp.Pages.Order.go(orderId);
-    },
-    memberAttending:function() {
-    	MeiweiApp.Pages.Attending.go();
-    }
+	restaurantDetail: function(rid) { MeiweiApp.Pages.RestaurantDetail.go(rid); },
+	restaurantSearch: function() { MeiweiApp.Pages.RestaurantSearch.go(); },
+	restaurantOrder: function(rid) { MeiweiApp.Pages.RestaurantOrder.go(rid); },
+	
+	memberCenter: function() { MeiweiApp.Pages.MemberCenter.go(); },
+	memberLogin: function() { MeiweiApp.Pages.MemberLogin.go(); },
+	memberProfile: function() { MeiweiApp.Pages.MemberProfile.go(); },
+    memberContacts: function() { MeiweiApp.Pages.MemberContacts.go(); },
+    
+    orderList: function() { MeiweiApp.Pages.OrderList.go(); },
+    orderDetail: function(orderId) { MeiweiApp.Pages.Order.go(orderId); },
+    
+    attending:function() { MeiweiApp.Pages.Attending.go(); }
 }));
 
 MeiweiApp.goTo = function(path) {
