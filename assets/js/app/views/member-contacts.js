1616990e9513925a0cdf508a2d35d5027615d310
eric <eric.wu@clubmeiwei.com>
2013-07-18 19:42:46 +0800

    从本地拿联系人

--- assets/js/app/views/member-contacts.js
+++ assets/js/app/views/member-contacts.js
@@ -1,41 +1,70 @@
-
 MeiweiApp.Views.ContactList = MeiweiApp.CollectionView.extend({
-	ModelView: MeiweiApp.ModelView.extend({
-		tagName: "div",
-		className: "contact-list-item",
-		events: {
-			"click": "updateContact"
-		},
-		template: Mustache.compile('<div><h1>{{name}}</h1></div><div><small>{{mobile}}</small></div>'),
-		updateContact: function(e) {
-			this.model.trigger("select");
-		}
-	})
+    ModelView: MeiweiApp.ModelView.extend({
+        tagName: "div",
+        className: "contact-list-item",
+        events: {
+            "click": "updateContact"
+        },
+        template: Mustache.compile('<div><h1>{{name}}</h1></div><div><small>{{mobile}}</small></div>'),
+        updateContact: function(e) {
+            this.model.trigger("select");
+        }
+    })
 });
 
-MeiweiApp.Pages.MemberContacts = new (MeiweiApp.PageView.extend({
-	initPage: function() {
-		this.views = {
-			contactList: new MeiweiApp.Views.ContactList({
-				collection: MeiweiApp.me.contacts,
-				el: this.$('.scroll-inner')
-			})
-		}
-		_.bindAll(this, "bindContactSelect");
-	},
-	bindContactSelect: function(collection, response, options) {
-		collection.forEach(
-			function(contact) {
-				contact.on("select", function() {
-					this.$('input[name=contactname]').val(contact.get('name'));
-					this.$('input[name=contactphone]').val(contact.get('mobile'));
-				}, this)
-			}, this);
-		collection.at(0).trigger("select");
-	},
-	render: function() {
-		$.when(
-			MeiweiApp.me.contacts.fetch({ reset: true, success: this.bindContactSelect })
-		).then(this.showPage);
-	}
-}))({el: $("#view-member-contacts")});
+MeiweiApp.Pages.MemberContacts = new(MeiweiApp.PageView.extend({
+    initPage: function() {
+        this.views = {
+            contactList: new MeiweiApp.Views.ContactList({
+                collection: new MeiweiApp.Collections.Contacts(),
+                el: this.$('.scroll-inner')
+            })
+        }
+        _.bindAll(this, "bindContactSelect");
+    },
+    bindContactSelect: function(collection, response, options) {
+        collection.forEach(function(contact) {
+            contact.on("select",
+            function() {
+                this.$('input[name=contactname]').val(contact.get('name'));
+                this.$('input[name=contactphone]').val(contact.get('mobile'));
+            },
+            this)
+        },
+        this);
+        collection.at(0).trigger("select");
+    },
+
+    getMobileContacts: function() {
+        var collection = new MeiweiApp.Collections.Contacts();
+        try {
+            var self = this;
+            navigator.contacts.find(["displayName", "phoneNumbers"],
+            function(contacts) {
+                for (i = 0; i < contacts.length; i++) {
+                    var model = new MeiweiApp.Models.Contact();
+                    model.set({
+                        id: contacts[i].id,
+                        name: contacts[i].displayName,
+                        mobile: contacts[i].phoneNumbers ? contacts[i].phoneNumbers[0].value: ""
+                    });
+                    collection.add(model);
+                }
+                self.views.contactList.collection.reset(collection.models);
+                self.views.contactList.render();
+            },
+            function(e) {},
+            {
+                multiple: true
+            });
+
+        } catch(e) {};
+    },
+
+    render: function() {
+        $.when(this.getMobileContacts()).then(this.showPage);
+        //this.bindContactSelect(collection);
+    }
+}))({
+    el: $("#view-member-contacts")
+});
--- assets/js/plugin/util.js
+++ assets/js/plugin/util.js
@@ -1,56 +1,30 @@
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        
-        $(".begin").css( "color" , "red" );
-        MeiweiApp.googleMap.getLocation();
-
-        
-        //window.plugins.statusBarNotification.notify("Put your title here", "Put your sticky message here", Flag.FLAG_NO_CLEAR);
-        
+$.extend(MeiweiApp, {
+    googleMap: {
+        getLocation: function() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log(position);
+                alert(position.coords.latitude);
+            },
+            function(e) {
+                console.log(e);
+            },
+            {
+                enableHighAccuracy: true,
+                maximumAge: 30000,
+                timeout: 27000
+            });
+        }
     }
-};
+});
 
+//MeiweiApp.googleMap.getLocation();
+navigator.contacts.find(["displayName", "phoneNumbers"],
+function(contacts) {
 
-
-$.extend( MeiweiApp, {
-	googleMap : { 
-		getLocation : function(){
-			navigator.geolocation.getCurrentPosition( function( position ){
-				console.log(position);
-				alert(position.coords.latitude);
-			} ,function( e ){
-				console.log(e);
-			} , { enableHighAccuracy:true,   maximumAge:30000,  timeout:27000});
-		}
-	}
-}); 
-
-//app.initialize();
+    for (var i = 0; i < contacts.length; i++) {
+        //alert("Display Name = " + contacts[i].displayName + ":" + contacts.phoneNumbers);
+    }
+},
+function(e) {},
+{});
+//window.plugins.statusBarNotification.notify("Put your title here", "Put your sticky message here", Flag.FLAG_NO_CLEAR);
