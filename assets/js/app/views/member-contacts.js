e05f903584997d769883c6bb0ed106b300c86cb1
Xindong Ding <dxd.spirits@gmail.com>
2013-07-23 00:34:16 +0800

    Put online&local contacts in the same page.

--- assets/js/app/views/member-contacts.js
+++ assets/js/app/views/member-contacts.js
@@ -4,98 +4,97 @@ d = d.getTime() + 60 * 1000; //60 seconds from now
 d = new Date(d);
 
 window.addNotification({
-    fireDate: d,
-    alertBody: "This is a local notification.",
-    repeatInterval: "daily",
-    soundName: "horn.caf",
-    badge: 0,
-    notificationId: 123,
-    foreground: function(notificationId) {
-        alert("Hello World! This alert was triggered by notification " + notificationId);
-        console.log(notificationId);
-    },
-    background: function(notificationId) {
-        alert("Hello World! This alert was triggered by notification " + notificationId);
-        console.log(notificationId);
+	fireDate: d,
+	alertBody: "This is a local notification.",
+	repeatInterval: "daily",
+	soundName: "horn.caf",
+	badge: 0,
+	notificationId: 123,
+	foreground: function(notificationId) {
+		alert("Hello World! This alert was triggered by notification " + notificationId);
+		console.log(notificationId);
+	},
+	background: function(notificationId) {
+		alert("Hello World! This alert was triggered by notification " + notificationId);
+		console.log(notificationId);
 
-    }
+	}
 })
 
 
 
-
-
-
-
 MeiweiApp.Views.ContactList = MeiweiApp.CollectionView.extend({
-    ModelView: MeiweiApp.ModelView.extend({
-        tagName: "div",
-        className: "simple-list-item",
-        events: {
-            "click": "updateContact"
-        },
-        template: Mustache.compile('<div class="span"><h1>{{name}}</h1></div><div class="span"><small>{{mobile}}</small></div>'),
-        updateContact: function(e) {
-            this.model.trigger("select");
-        }
-    })
+	ModelView: MeiweiApp.ModelView.extend({
+		tagName: "div",
+		className: "simple-list-item",
+		events: { "click": "triggerSelect" },
+		template: Mustache.compile('<div class="span"><h1>{{name}}</h1></div><div class="span"><small>{{mobile}}</small></div>'),
+		triggerSelect: function(e) {
+			this.model.trigger("select");
+		}
+	})
 });
 
 MeiweiApp.Pages.MemberContacts = new(MeiweiApp.PageView.extend({
-    initPage: function() {
-        this.views = {
-            contactList: new MeiweiApp.Views.ContactList({
-                collection: new MeiweiApp.Collections.Contacts(),
-                el: this.$('.scroll-inner')
-            })
-        }
-        _.bindAll(this, "bindContactSelect");
-    },
-    bindContactSelect: function(collection, response, options) {
-        collection.forEach(function(contact) {
-            contact.on("select",
-            function() {
-                this.$('input[name=contactname]').val(contact.get('name'));
-                this.$('input[name=contactphone]').val(contact.get('mobile'));
-            },
-            this)
-        },
-        this);
-        collection.at(0).trigger("select");
-    },
-
-    getMobileContacts: function(callback) {
-        var collection = this.views.contactList.collection;
-        try {
-            var self = this;
-            navigator.contacts.find(["displayName", "phoneNumbers"],
-            function(contacts) {
-                for (i = 0; i < contacts.length; i++) {
-                    var model = new MeiweiApp.Models.Contact();
-                    model.set({
-                        id: contacts[i].id,
-                        name: contacts[i].displayName,
-                        mobile: contacts[i].phoneNumbers ? contacts[i].phoneNumbers[0].value: ""
-                    });
-                    collection.add(model);
-                }
-                if (typeof callback == 'function') {
-                    callback.call( self );
-                }
-            },
-            function(e) {},
-            {
-                multiple: true
-            });
-
-        } catch(e) {};
-    },
-
-    render: function() {
-        this.getMobileContacts(function() {
-            this.showPage();
-        });
-    }
+	events: {
+		'click .filter-bar :nth-child(1)': 'selectOnline',
+		'click .filter-bar :nth-child(2)': 'selectLocal',
+		'click simple-list-item': 'confirmSelect'
+	},
+	selectOnline: function() { this.getOnlineContacts(); },
+	selectLocal: function() { this.getLocalContacts(); },
+	confirmSelect: function() {  },
+	initPage: function() {
+		this.views = {
+			contactList: new MeiweiApp.Views.ContactList({
+				collection: new MeiweiApp.Collections.Contacts(),
+				el: this.$('.scroll-inner')
+			})
+		}
+		_.bindAll(this, "bindContactSelect");
+	},
+	bindContactSelect: function(collection, response, options) {
+		collection.forEach(function(contact) {
+			contact.on("select",
+			function() {
+				this.$('input[name=contactname]').val(contact.get('name'));
+				this.$('input[name=contactphone]').val(contact.get('mobile'));
+			},
+			this)
+		},
+		this);
+		collection.at(0).trigger("select");
+	},
+	
+	getLocalContacts: function(callback) {
+		var collection = this.views.contactList.collection;
+		var self = this;
+		navigator.contacts.find(
+			["displayName", "phoneNumbers"],
+			function(contacts) {
+				for (i = 0; i < contacts.length; i++) {
+					var model = new MeiweiApp.Models.Contact();
+					model.set({
+						id: contacts[i].id,
+						name: contacts[i].displayName,
+						mobile: contacts[i].phoneNumbers ? contacts[i].phoneNumbers[0].value: ""
+					});
+					collection.add(model);
+				}
+			},
+			function(e) {},
+			{ multiple: true }
+		);
+	},
+	
+	getOnlineContacts: function() {
+		this.views.contactList.collection.fetch({ reset: true });
+	},
+	
+	render: function() {
+		this.getOnlineContacts();
+		this.showPage();
+	}
 }))({
-    el: $("#view-member-contacts")
+	el: $("#view-member-contacts")
 });
