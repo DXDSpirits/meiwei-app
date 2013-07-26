7258e24ebcdd8e1d0c14273fd3baf645111714f5
Xindong Ding <dxd.spirits@gmail.com>
2013-07-26 17:53:34 +0800

    Add fav resto removal.

--- assets/js/app/views/member-favorites.js
+++ assets/js/app/views/member-favorites.js
@@ -2,12 +2,18 @@
 MeiweiApp.Views.FavoriteList = MeiweiApp.CollectionView.extend({
 	ModelView: MeiweiApp.ModelView.extend({
 		template: MeiweiApp.Templates['favorite-list-item'],
-		events: { 'click': 'viewRestaurant' },
+		events: {
+			'click section': 'viewRestaurant',
+			'click .delete-button': 'deleteFav',
+		},
 		className: 'restaurant-list-item',
 		viewRestaurant: function() {
 			MeiweiApp.goTo('RestaurantDetail', {
 				restaurantId: this.model.get('restaurant')
 			});
+		},
+		deleteFav: function(e) {
+			this.model.destroy();
 		}
 	})
 });
