ec0c482535e75db0ef35aaf0f211583fed517e5f
Xindong Ding <dxd.spirits@gmail.com>
2013-07-28 22:47:50 +0800

    Add cancel action to Order.

--- assets/js/app/models/orders.js
+++ assets/js/app/models/orders.js
@@ -1,6 +1,10 @@
 
 MeiweiApp.Models.Order = MeiweiApp.Model.extend({
-	urlRoot: MeiweiApp.configs.APIHost + '/orders/order/'
+	urlRoot: MeiweiApp.configs.APIHost + '/orders/order/',
+	cancel: function() {
+		var url = this.url() + 'cancel/';
+		Backbone.sync('update', this, {url: url});
+	}
 });
 
 MeiweiApp.Collections.Orders = MeiweiApp.Collection.extend({
