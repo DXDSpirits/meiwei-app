a00ab6cd35c1996f212cb74868856d11bd60aa69
Xindong Ding <dxd.spirits@gmail.com>
2013-07-31 17:01:02 +0800

    Set table color in css rather than in js. Fix some bug on floorplan page.

--- assets/js/app/views/restaurant-floorplans.js
+++ assets/js/app/views/restaurant-floorplans.js
@@ -18,23 +18,14 @@ MeiweiApp.Views.FloorplanNav = MeiweiApp.CollectionView.extend({
 });
 
 MeiweiApp.Views.FloorplanList = MeiweiApp.CollectionView.extend({
-	// getIdVar: function(){
-		// var tables = [];
-		// $.each(this.modelViews , function(){
-			// var idVar = "\""+ this.model.get('id')+"\"";
-			// var _ids = this.getId();
-			// if(_ids && _ids.length>0){
-				// idVar+= ":\""+_ids.join(",")+"\"";
-				// tables.push(idVar);
-			// }
-		// });
-		// return "{"+tables.join(",")+"}";
-	// },
 	ModelView: MeiweiApp.ModelView.extend({
 		events: {'click .table':'selectSeat'},
 		initialize: function() {
 			this.seatMap = {};
-			this.options={colorAvailable : "#669933",colorSelected : "#77513D",colorOccupied : "#E6E6E6" , maxSeat: 1 , size: 0};
+			this.options = {
+				maxSeat: 1,
+				size: 0
+			};
 			this.listenTo(this.model, 'select', this.onSelect);
 		},
 		onSelect: function() {
@@ -47,60 +38,25 @@ MeiweiApp.Views.FloorplanList = MeiweiApp.CollectionView.extend({
 			var $t = $(e.target);
 			var id = this.model.get("id");
 			
-			if($t.attr("status")=="available"){
+			if($t.attr("status")=="available") {
 				if( this.options.size >= this.options.maxSeat){
 					if(window.confirm("很抱歉，你所选桌位已超出预设数目！\n点击确认重新选择。")){
-						this.$el.find("[status=selected]").attr("status","available").attr("fill", this.options.colorAvailable);
+						this.$el.find("[status=selected]").attr("status","available");
 						this.seatMap = {};
 						this.options.size = 0;
 					}
 					return;
 				}
-				$t.attr("status","selected").attr("fill", this.options.colorSelected);
+				$t.attr("status","selected");
 				this.options.size +=1;
 				if(!this.seatMap[id]) this.seatMap[id] = {};
 				this.seatMap[id][$t.attr("tableid")] = null;
-			}else if($t.attr("status")=="selected"){
-				$t.attr("status","available").attr("fill", this.options.colorAvailable);
+			} else if ($t.attr("status")=="selected"){
+				$t.attr("status","available");
 				this.options.size-=1;
 				delete this.seatMap[id][$t.attr("tableid")];
 			}
 		},
-		// getId: function(){
-			// this.idArray = [];
-			// var $this = this;
-			// this.each( function(item){
-				// if($(item).attr("status")=="selected"){
-					// $this.idArray.push($(item).attr("tableid"));
-				// }
-			// });
-			// return this.idArray;
-		// },
-		// initSeatMap: function(){
-			// this.seatMap = $.parseJSON( null ) || {};
-		// },
-		// lightSeat: function(){
-			// var idVar = this.seatMap[this.model.get('id')];
-			// if(!idVar) return;
-// 			
-			// var $this = this;
-			// this.each( function(item){
-				// if(idVar.match($(item).attr('tableid'))){
-					// $(item).attr("status","selected");
-					// $(item).attr("fill", $this.options.colorSelected);
-				// }else{
-					// if($(item).attr("status")!="occupied"){
-						// $(item).attr("status","available");
-						// $(item).attr("fill", $this.options.colorAvailable);
-					// }
-				// }
-			// });
-		// },
-		// each: function(callback){
-			// this.$el.find("[class=table]").each(function(){
-				// callback(this);
-			// });
-		// },
 		render: function() {
 			var plan = this.model;
 			var self = this;
@@ -109,8 +65,7 @@ MeiweiApp.Views.FloorplanList = MeiweiApp.CollectionView.extend({
 				$(svg).attr('width', '100%');
 				$(svg).attr('height', '400px');
 				self.$el.html(svg);
-				self.model.trigger('select');			
-				//self.lightSeat();
+				self.model.trigger('select');
 			});
 			return this;
 		}
@@ -154,8 +109,6 @@ MeiweiApp.Pages.RestaurantFloorplans = new (MeiweiApp.PageView.extend({
 	},
 	render: function() {
 		this.floorplans.reset(this.options.floorplans.models);
-		this.views.floorplanNav.render();
-		this.views.floorplanList.render();
 		this.renderFloorplans();
 		this.showPage();
 	}
