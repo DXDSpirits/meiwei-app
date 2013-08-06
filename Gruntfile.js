e87f36b9d7ae2e92f37d836720902acef15c6846
Xindong Ding <dxd.spirits@gmail.com>
2013-08-06 14:02:05 +0800

    Remove unuseful js files. Move BMap to local. Add bootstrap.js to accelerate the initial loading.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,13 +14,14 @@ module.exports = function(grunt) {
 				files: {
 					'assets/js/mw-app.js': [
 						'assets/js/app/app.js',
-						'assets/js/app/app-local.js',
 						'assets/js/app/templates.js',
 						'assets/js/app/model.js',
 						'assets/js/app/models/*.js',
 						'assets/js/app/view.js',
 						'assets/js/app/views/*.js',
-						'assets/js/app/router.js'
+						'assets/js/app/router.js',
+						'assets/js/app/bootstrap.js',
+						'assets/js/app/app-local.js'
 					]
 				}
 			}
