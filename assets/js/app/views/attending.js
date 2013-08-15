14922ff868c509df3fc5f84aaa08c6d65027147b
Xindong Ding <dxd.spirits@gmail.com>
2013-08-15 20:48:49 +0800

    Modify qrcode and share button on order-attending page.

--- assets/js/app/views/attending.js
+++ assets/js/app/views/attending.js
@@ -1,33 +1,32 @@
 
 MeiweiApp.Views.OrderPages = MeiweiApp.CollectionView.extend({
 	ModelView: MeiweiApp.ModelView.extend({
-		events: {'click button.btn-share' : 'onClickShareBtn' },
+		events: {'click .btn-share' : 'onClickShareBtn' },
 		template: MeiweiApp.Templates['order-attending'],
 		className: 'order-page-item carousel-item',
 		getWeiboLink : function(s, d, e, r, l, p, t, z, c) {
-		    var f = 'http://v.t.sina.com.cn/share/share.php?appkey=',
-		    u = z || d.location,
-		    p = ['&url=', e(u), '&title=', e(t || d.title), '&source=', e(r), '&sourceUrl=', e(l), '&content=', c || 'gb2312', '&pic=', e(p || '')].join('');
+		    var f = 'http://v.t.sina.com.cn/share/share.php?appkey=', u = z || d.location,
+		    p = ['&url=', e(u), '&title=', e(t || d.title), '&source=', e(r), '&sourceUrl=', e(l), 
+		    	'&content=', c || 'gb2312', '&pic=', e(p || ''), '&ralateUid=', '3058840707'].join('');
 		    return [f, p].join('');
 		},
 		onClickShareBtn : function(){
- 	 		var content = this.model.get('restaurantinfor').fullname;
-	 		if(content && content.length>120){
-	 			content = content.substring(0,120);
-	 		}
+ 	 		var restaurantinfor = this.model.get('restaurantinfor');
+ 	 		var content  = '我在 #' + restaurantinfor.fullname + ' (' + restaurantinfor.address + ') ';
 	 		var pic =  this.model.get('restaurantinfor').frontpic;
-	 		var link = this.getWeiboLink(screen, document, encodeURIComponent, '', '', 
-	 		pic, content, '', 'utf-8');
+	 		var link = this.getWeiboLink(screen, document, encodeURIComponent, 'http://www.clubmeiwei.com', 'http://www.clubmeiwei.com', 
+	 									pic, content, 'http://www.clubmeiwei.com/restaurant/view/' + this.model.get('restaurant'), 'utf-8');
 	 		var ref = window.open( link ,'_blank', 'location=no');
 		},
 		render: function() {
 			this.$el.html(this.template(this.model.toJSON()));
-			this.$('.section-qrcode').qrcode({
-				render	: "canvas",
-				text	: this.model.get('orderno'),
-				height: 130,
-				width:  130
-			});	
+			if ($('body').height() > 555) {
+				this.$('.section-qrcode').show();
+				this.$('.section-qrcode').qrcode({
+					render: "canvas", text: this.model.get('orderno'), height: 130, width: 130
+				});
+				this.$('.order-detail').css({ 'position': 'relative', 'top': '-50px' });
+			}
 			return this;
 		}
 	})
