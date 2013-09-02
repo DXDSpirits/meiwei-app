e4ff3c84a1e9950e04c54240e74a6e9ae3a1656e
Xindong Ding <dxd.spirits@gmail.com>
2013-09-03 02:36:56 +0800

    Optimize page and templates;

--- assets/js/app/views/member-center.js
+++ assets/js/app/views/member-center.js
@@ -1,21 +1,32 @@
 
-MeiweiApp.Views.MemberProfileBox = MeiweiApp.ModelView.extend({
-	template: MeiweiApp.Templates['member-profile-box'],
+MeiweiApp.Views.MemberProfileBox = MeiweiApp.View.extend({
 	render: function() {
-		this.$el.html(this.template(this.model.toJSON()));
+		MeiweiApp.me.fetch({
+			success: function(model, response, options) {
+				var profile = MeiweiApp.me.profile.toJSON();
+				options.view.$('[data-field=nickname]').html(profile.nickname);
+				options.view.$('[data-field=mobile]').html(profile.mobile);
+				options.view.$('[data-field=email]').html(profile.email);
+			}, view: this
+		});
+		return this;
 	}
 });
 
 
 MeiweiApp.Views.FavoriteRestoCarousel = MeiweiApp.View.extend({
 	render: function() {
-		var path = 'assets/img/default.png';
-		if (this.collection.length > 0) {
-			var ran = _.random(0, this.collection.length - 1);
-			var model = this.collection.at(ran);
-			path = model.get('restaurantinfor').frontpic;
-		}
-		this.$el.html($('<img></img>').attr('src', path));
+		this.collection.fetch({
+			success: function(collection, response, options) {
+				var path = 'assets/img/default.png';
+				if (collection.length > 0) {
+					var ran = _.random(0, collection.length - 1);
+					var model = collection.at(ran);
+					path = model.get('restaurantinfor').frontpic;
+				}
+				options.view.$el.html($('<img></img>').attr('src', path));
+			}, reset: true, view: this 
+		});
 		return this;
 	}
 });
@@ -55,14 +66,7 @@ MeiweiApp.Pages.MemberCenter = new (MeiweiApp.PageView.extend({
 		}
 	},
 	render: function() {
-		var self = this;
-		MeiweiApp.me.fetch({success: function() {
-			self.favorites.fetch({
-				reset: true,
-				success: function() {
-					self.views.favoriteCarousel.render();
-				}
-			});
-		}});
+		this.views.profileBox.render();
+		this.views.favoriteCarousel.render();
 	}
 }))({el: $("#view-member-center")});
