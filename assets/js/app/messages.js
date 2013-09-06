a5d8a9d4621ed01eb914649b67880006294c4dc5
Xindong Ding <dxd.spirits@gmail.com>
2013-09-06 18:19:25 +0800

    Add messages.js

---
+++ assets/js/app/messages.js
@@ -0,0 +1,23 @@
+MeiweiApp._i = function(lang) {
+	//this.get_msg(lang);
+	this.messages = {
+		title: {
+			Attending: {zh: '签到', en: 'Checkin'},
+			MemberCenter: {zh: '用户中心', en: 'Member'},
+			MemberContacts: {zh: '联系人', en: 'Contacts'},
+			MemberCredits: {zh: '积分', en: 'Credits'},
+			MemberFavorites: {zh: '收藏', en: 'Favorites'},
+			MemberLogin: {zh: '登录', en: 'Login'},
+			MemberProfile: {zh: '我的资料', en: 'Profile'},
+			OrderDetail: {zh: '订单详情', en: 'Order Detail'},
+			OrderList: {zh: '订单', en: 'Orders'},
+			ProductPurchase: {zh: '增值服务', en: 'Services'},
+			ProductRedeem: {zh: '积分兑换', en: 'Rewards'},
+			RestaurantFloorplans: {zh: '选择桌位', en: 'Select Seat'},
+			RestaurantOrder: {zh: '订餐', en: 'Reserve'}
+		},
+		form: {
+			
+		}
+	};
+};
