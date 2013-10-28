9042b29970f48537186bf3483943cc2ac8d795b8
Xindong Ding <dxd.spirits@gmail.com>
2013-10-28 17:53:28 +0800

    Add android backbutton support

--- assets/js/app/_close.js
+++ assets/js/app/_close.js
@@ -1,2 +1,7 @@
     document.addEventListener("deviceready", MeiweiApp.start, false);
+    document.addEventListener("backbutton", function() {
+        if (MeiweiApp.history.active && MeiweiApp.history.active.onClickLeftBtn) {
+            MeiweiApp.history.active.onClickLeftBtn();
+        } 
+    false);
 });
