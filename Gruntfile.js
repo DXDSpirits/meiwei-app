b6177bea9e5270342829ea15c20b7dd82c4fca34
Xindong Ding <dxd.spirits@gmail.com>
2013-11-21 15:44:15 +0800

    re-organize project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,7 @@
 module.exports = function(grunt) {
-	var _ = grunt.util._;
-	var locales = ["zh-CN"];
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		cfg: grunt.file.readJSON('config.json'),
 		concat: {
 		    mwapp: {
                 src: [
@@ -26,8 +25,8 @@ module.exports = function(grunt) {
 		uglify: {
 			mwapp: {
 				options: {
-					sourceMap: 'assets/js/mw-app-min.map',
-					sourceMappingURL: 'mw-app-min.map',
+					sourceMap: '<%= cfg.path.dest %>/assets/js/mw-app.min.map',
+					sourceMappingURL: 'mw-app.min.map',
 					sourceMapPrefix: 2,
 					mangle: false,
 					beautify: {
@@ -35,80 +34,66 @@ module.exports = function(grunt) {
 						beautify: true
 					}
 				},
-				files: {
-					'assets/js/mw-app-min.js': [
-						'assets/js/mw-app.js'
-					]
-				}
+				src: 'assets/js/mw-app.js',
+				dest: '<%= cfg.path.dest %>/assets/js/mw-app.min.js'
 			},
-			iscroll: {
-				options: {
-					sourceMap: 'assets/js/plugin/iscroll/iscroll-min.map',
-					sourceMappingURL: 'iscroll-min.map',
-					sourceMapPrefix: 4,
-				},
-				files: {
-					'assets/js/plugin/iscroll/iscroll-min.js': [
-						'assets/js/plugin/iscroll/iscroll.js'
-					]
-				}
+			plugins: {
+				files: [
+					{
+						src: 'assets/js/plugin/helper.js',
+						dest: '<%= cfg.path.dest %>/assets/js/plugin/helper.min.js'
+					},{
+						src: 'assets/js/plugin/plugins.js',
+						dest: '<%= cfg.path.dest %>/assets/js/plugin/plugins.min.js'
+					},{
+						src: ['assets/js/plugin/qrcode.js', 'assets/js/plugin/jquery.qrcode.js'],
+						dest: '<%= cfg.path.dest %>/assets/js/plugin/qrcode.min.js'
+					},{
+						src: 'assets/js/plugin/iscroll.js',
+						dest: '<%= cfg.path.dest %>/assets/js/plugin/iscroll.min.js'
+					}
+				]
 			}
 		},
 		sass: {
 			dist: {
-				files: {
-					'assets/css/mobile.css': 'assets/scss/mobile.scss'
-				}
+				src: 'assets/scss/mobile.scss',
+				dest: '<%= cfg.path.dest %>/assets/css/mobile.css'
 			}
 		},
 		includes: {
 			files: {
 				src: ['mobile/index.html'],
-				dest: '.',
+				dest: '<%= cfg.path.dest %>',
 				flatten: true,
 				cwd: '.'
 			}
 		},
 		templates: {
 			all: {
-				files: {
-					'assets/js/app/templates.js': [
-						'assets/template/*.html',
-						'assets/template/*/*.html'
-					]
-				}
+				src: ['assets/template/*.html', 'assets/template/*/*.html'],
+				dest: 'assets/js/app/templates.js'
 			}
 		},
-		xgettext : {
-			options : {
-				functionName : "_i",
-				potFile : "locale/messages.pot",
-				//processMessage: function(message) { ... }
-			},
-			target : {
-				files : {
-					handlebars : [],
-					javascript : ['assets/js/mw-app.js']
-				}
+		copy: {
+			staticfiles: {
+				expand: true,
+    			src: ['assets/img/**/*', 'assets/font/**/*', 'assets/js/vendor/**/*', 'config.xml'],
+    			dest: '<%= cfg.path.dest %>'
 			}
-		},
-		po2json: {
-			target: { 
-				files: _.object(
-					_.map(locales, function(locale) { return "locale/" + locale + ".js"; }),
-					_.map(locales, function(locale) { return "locale/" + locale + ".po"; })
-				)
-			}, 
-			options: { requireJs: true }
 		},
 		watch: {
 			scripts_mwapp: {
 				files: ['assets/js/app/*.js', 'assets/js/app/models/*.js', 'assets/js/app/views/*.js'],
 				tasks: ['concat:mwapp', 'uglify:mwapp']
 			},
-			scripts_iscroll: {
-				files: ['assets/js/plugin/iscroll/iscroll.js'],
-				tasks: ['uglify:iscroll']
+			scripts_plugins: {
+				files: ['assets/js/plugin/plugin/*.js'],
+				tasks: ['uglify:plugins']
+			},
+			staticfiles: {
+				files: ['assets/img/**/*', 'assets/font/**/*', 'assets/js/vendor/**/*', 'config.xml'],
+				tasks: ['copy']
 			},
 			stylesheets: {
 				files: ['assets/scss/*.scss', 'assets/scss/mobile/*.scss', 'assets/scss/font-awesome/*.scss', 'assets/scss/pages/*.scss'],
@@ -127,6 +112,7 @@ module.exports = function(grunt) {
 			server: {
 				options: {
 					port: 8080,
+					base: '<%= cfg.path.dest %>',
 					keepalive: true,
 					hostname: null,
 					middleware: function(connect, options){
@@ -149,7 +135,7 @@ module.exports = function(grunt) {
 		},
 		concurrent: {
 		    dist: {
-                tasks: ['sass', 'includes', 'templates', 'concat', 'uglify'],
+                tasks: ['sass', 'includes', 'templates', 'concat', 'uglify', 'copy'],
                 options: { logConcurrentOutput: true }
             },
 			server: {
@@ -158,31 +144,12 @@ module.exports = function(grunt) {
 					logConcurrentOutput: true
 				}
 			}
-		},
-		shell: {
-			options: {
-				failOnError: true,
-				stdout: true
-			},
-			msgmerge: {
-				command: _.map(locales, function(locale) {
-					var po = "locale/" + locale + ".po";
-					return "if [ -f \"" + po + "\" ]; then\n" +
-						   "    echo \"Updating " + po + "\"\n" +
-						   "    msgmerge " + po + " locale/messages.pot > .new.po.tmp\n" +
-						   "    if [ $? -ne 0 ]; then\n" +
-						   "        echo \"Msgmerge failed with exit code $?\"\n" +
-						   "        exit $?\n" +
-						   "    fi\n" +
-						   "    mv .new.po.tmp " + po + "\n" +
-						   "fi\n";
-				}).join("")
-			}
 		}
 	});
 	
 	grunt.loadTasks('tasks');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
@@ -191,12 +158,11 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-shell');
-
+	
 	// Configurable port number
 	var port = grunt.option('port');
 	if (port) grunt.config('connect.server.options.port', port);
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.registerTask('server', 'concurrent:server');
 	grunt.registerTask('dist', 'concurrent:dist');
-
 };
