2065ad1bc821dd7fe0af1fac4864c26b31f0f79e
Xindong Ding <dxd.spirits@gmail.com>
2013-11-27 10:24:46 +0800

    optimize getstarted page. render images (background-image) as needed. clear carousel-items after leaving (never needed before next update).

--- assets/js/app/views/getstarted.js
+++ assets/js/app/views/getstarted.js
@@ -1,16 +1,26 @@
 $(function() {
     MeiweiApp.Pages.GetStarted = new (MeiweiApp.PageView.extend({
+    	initPage: function() {
+    		this.pics = [
+    			'assets/img/getstarted/1@640x1096.png',
+    			'assets/img/getstarted/2@640x1096.png',
+    			'assets/img/getstarted/3@640x1096.png',
+    			'assets/img/getstarted/4@640x1096.png',
+    			'assets/img/getstarted/5@640x1096.png'
+    		];
+    	},
         events: {
             'tap .carousel-item.getstarted': 'getstarted'
         },
         getstarted: function() {
             localStorage.setItem('first-time', true);
             MeiweiApp.goTo('Home');
+            this.scroller.destroy();
+            this.undelegateEvents();
+            this.$('.carousel-inner').empty();
         },
-        render: function() {
-            var items = this.$('.carousel > .carousel-inner > .carousel-item');
-            this.$('.carousel > .carousel-inner').css('width', items.length * $(items[0]).outerWidth());
-            if (this.scroller == null) {
+        initScroller: function() {
+        	if (this.scroller == null) {
                 if (this.$('.carousel').length > 0) {
                     this.scroller = new IScroll(this.$('.carousel').selector, {
                         scrollX: true, scrollY: false, momentum: false, snap: true, tap: true,
@@ -23,6 +33,27 @@ $(function() {
             } else {
                 this.scroller.refresh();
             }
+        },
+        fillImages: function() {
+        	var $list = [];
+        	for (var i=0; i<this.pics.length; i++) {
+        		$list.push($('<div></div>').addClass('carousel-item').css('background-image', 'url(' + this.pics[i] + ')'));
+        	}
+        	$list[$list.length-1].addClass('getstarted');
+        	this.$('.carousel-inner').html($list);
+        },
+        adjustWidth: function() {
+        	var bodyWidth = $('body').innerWidth();
+            var items = this.$('.carousel > .carousel-inner > .carousel-item');
+            for (var i=0; i<items.length; i++) {
+            	$(items[i]).css('width', bodyWidth);
+            }
+            this.$('.carousel > .carousel-inner').css('width', items.length * $(items[0]).outerWidth());
+        },
+        render: function() {
+        	this.fillImages();
+        	this.adjustWidth();
+            this.initScroller();
         }
     }))({el: $("#view-getstarted")});
 });
