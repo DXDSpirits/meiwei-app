edba652d329d7dfaf68ee2f43a0c9a18d8433ee0
Xindong Ding <dxd.spirits@gmail.com>
2013-12-13 10:10:45 +0800

    update bootstrapping data (add ids to heros), fix click event on first-visit hover on homepage.

--- assets/js/app/views/home.js
+++ assets/js/app/views/home.js
@@ -155,7 +155,7 @@ $(function() {
             var key = 'visited-view-home';
             if (!localStorage.getItem(key)) {
                 this.$el.addClass('first-visit');
-                this.$el.one('touchstart', function(e) {
+                this.$el.one('touchstart click', function(e) {
                 	if (e.preventDefault) e.preventDefault();
                     $(this).removeClass('first-visit');
                     localStorage.setItem(key, true);
