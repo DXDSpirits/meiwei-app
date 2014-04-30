a207ff389a1da560309a48f55e8a4e28a96ab3f7
kinglio8520 <kinglio8520@gmail.com>
2014-04-30 19:34:58 +0800

    remove test code.

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -54,10 +54,6 @@ MeiweiApp.Router = new (Backbone.Router.extend({
 	},
 	
 	registerAuthToken: function(token, path) {
-        var index = path.indexOf("&");
-        if(index>0) {
-            path = path.substr(0,index);
-        }
 	    MeiweiApp.TokenAuth.set(token);
 	    this.navigate(path, {trigger: true});
 	},
