910969a75924c2f8376240de1485ea95d7412925
kinglio8520 <kinglio8520@gmail.com>
2014-06-16 16:20:17 +0800

    add payment status in order-detail.html.

--- assets/js/app/models/orders.js
+++ assets/js/app/models/orders.js
@@ -5,7 +5,11 @@ MeiweiApp.Models.Order = MeiweiApp.Model.extend({
 		response.ordertime = response.ordertime.slice(0, 5);
 		response.editable = (response.status < 20);
         response.is_payable = false;
+        response.is_prepay = false;
         if(response.payment_order) {
+            if(response.payment_order.amount>0) {
+                response.is_prepay = true;
+            }
             if(response.payment_order.status==0) {
                 response.is_payable = true;
             }
