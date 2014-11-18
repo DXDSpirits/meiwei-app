a951dd80942c6184353c8b1b3dcff05105e9affa
Xindong Ding <dxd.spirits@gmail.com>
2014-11-18 11:44:22 +0800

    upgrade model and collection def

--- assets/javascripts/app/model.js
+++ assets/javascripts/app/model.js
@@ -16,7 +16,27 @@ MeiweiApp.initCache = function(model, options) {
 	};
 };
 
+MeiweiApp.Model = Backbone.Model.extend({
+    fetch: function(options) {
+        options = options || {};
+        //MeiweiApp.initCache(this, options);
+        var self = this;
+        _.delay(function() {
+            Backbone.Model.prototype.fetch.call(self, options);
+        }, options.delay || 0);
+    },
+    url: function() {
+        if (this.attributes.url) {
+            return this.attributes.url;
+        } else {
+            var origUrl = Backbone.Model.prototype.url.call(this);
+            return origUrl + (origUrl.charAt(origUrl.length - 1) == '/' ? '' : '/');
+        }
+    }
+});
+
 MeiweiApp.Collection = Backbone.Collection.extend({
+    model: MeiweiApp.Model,
 	parse: function(response) {
 		if (response.results != null) {
 			this.count = response.count;
@@ -48,36 +68,9 @@ MeiweiApp.Collection = Backbone.Collection.extend({
 		options = options || {};
 		options.reset = options.reset || this.isEmpty();
 		//MeiweiApp.initCache(this, options);
-		if (options.delay) {
-		    var self = this;
-		    setTimeout(function() {
-		       Backbone.Collection.prototype.fetch.call(self, options); 
-		    }, options.delay);
-		} else {
-		    return Backbone.Collection.prototype.fetch.call(this, options);
-		}
+		var self = this;
+        _.delay(function() {
+           Backbone.Collection.prototype.fetch.call(self, options); 
+        }, options.delay || 0);
 	},
-});
-
-MeiweiApp.Model = Backbone.Model.extend({
-	fetch: function(options) {
-		options = options || {};
-		//MeiweiApp.initCache(this, options);
-		if (options.delay) {
-		    var self = this;
-		    setTimeout(function() {
-		        Backbone.Model.prototype.fetch.call(self, options);
-		    }, options.delay);
-		} else {
-		    return Backbone.Model.prototype.fetch.call(this, options);
-		}
-	},
-	url: function() {
-		if (this.attributes.url) {
-			return this.attributes.url;
-		} else {
-			var origUrl = Backbone.Model.prototype.url.call(this);
-			return origUrl + (origUrl.charAt(origUrl.length - 1) == '/' ? '' : '/');
-		}
-	}
 });
