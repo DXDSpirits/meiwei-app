d9cef416f136bae64276906578f7794877c54c9a
Xindong Ding <dxd.spirits@gmail.com>
2014-11-18 11:52:40 +0800

    upgrade views def

--- assets/javascripts/app/view.js
+++ assets/javascripts/app/view.js
@@ -1,7 +1,24 @@
 
 MeiweiApp.View = Backbone.View.extend({
-    initialize: function() {
-        if (this.initView) this.initView();
+    initialize: function(options) {
+        if (this.initView) this.initView(options || {});
+    },
+    renderTemplate: function(attrs, template) {
+        var template = template || _.result(this, 'template') || '';
+        var attrs = this.mixinTemplateHelpers(attrs);
+        this.$el.html(Mustache.render(template, attrs));
+        this.$el.find('img[data-src]').addBack('img[data-src]').each(function() {
+            MeiweiApp.loadImage($(this), $(this).data('src'));
+        });
+        this.$el.find('.img[data-bg-src]').addBack('.img[data-bg-src]').each(function() {
+            MeiweiApp.loadBgImage($(this), $(this).data('bg-src'));
+        });
+        MeiweiApp.initLang(this.$el);
+        return this;
+    },
+    mixinTemplateHelpers: function(target){
+        var target = target || {};
+        return _.extend(target, _.result(this, 'templateHelpers'));
     },
     displayError: function($el, text) {
         try {
@@ -46,68 +63,82 @@ MeiweiApp.View = Backbone.View.extend({
         } catch (e) {
             $form.find('.info-text').text(error || 'Error');
         }
-    },
-    
-    template: '',
-    renderTemplate: function(attrs) {
-        this.$el.html(Mustache.render(this.template, attrs || {}));
-        MeiweiApp.initLang(this.$el);
-        return this;
     }
 });
 
+
 MeiweiApp.ModelView = MeiweiApp.View.extend({
-    initView: function() {
-        if (this.model) {
-            this.listenTo(this.model, 'change', this.render);
-            this.listenTo(this.model, 'hide', this.hide);
-        }
-        if (this.initModelView) this.initModelView();
+    listenToModel: function() {
+        this.listenTo(this.model, 'change', this.render);
+        this.listenTo(this.model, 'hide', this.hide);
+    },
+    initView: function(options) {
+        this.model = this.model || new Model();
+        this.listenToModel();
+        if (this.initModelView) this.initModelView(options || {});
+    },
+    setModel: function(model) {
+        this.stopListening(this.model);
+        this.model = model;
+        this.listenToModel();
     },
     hide: function() {
-        var self = this;
         this.$el.animate({
             opacity: 0
         }, 1000, function() {
-            self.remove();
+            $(this).remove();
         });
     },
+    serializeData: function() {
+        return this.model ? this.model.toJSON() : {};
+    },
     render: function() {
-        var attrs = this.model ? this.model.toJSON() : {};
-        return this.renderTemplate(attrs);
+        return this.renderTemplate(this.serializeData());
     }
 });
 
+
 MeiweiApp.CollectionView = MeiweiApp.View.extend({
     ModelView: MeiweiApp.ModelView,
-    initView: function() {
-        if (this.collection) {
-            this.listenTo(this.collection, 'reset', this.addAll);
-            this.listenTo(this.collection, 'add', this.addOne);
-            this.listenTo(this.collection, 'remove', this.removeOne);
-        }
-        if (this.initCollectionView) this.initCollectionView();
+    listenToCollection: function() {
+        this.listenTo(this.collection, 'reset', this.addAll);
+        this.listenTo(this.collection, 'add', this.addOne);
+        this.listenTo(this.collection, 'remove', this.removeOne);
+    },
+    initView: function(options) {
+        options = options || {};
+        this.reverse = (options.reverse === true);
+        this.collection = this.collection || new Collection();
+        this.listenToCollection();
+        if (this.initCollectionView) this.initCollectionView(options);
+    },
+    setCollection: function(collection) {
+        this.stopListening(this.collection);
+        this.collection = collection;
+        this.listenToCollection();
+    },
+    renderItem: function(item) {
+        var modelView = new this.ModelView({model: item});
+        return modelView.render().el;
     },
     removeOne: function(item) {
         item.trigger('hide');
     },
     addOne: function(item) {
-        var modelView = new this.ModelView({model: item});
-        this.$el.append(modelView.render().el);
+        var method = this.reverse ? 'prepend' : 'append';
+        this.$el[method](this.renderItem(item));
     },
     addAll: function(_collection, options) {
-    	if (options && options.previousModels) {
-	    	_.each(options.previousModels, function(model) {
-	       		model.trigger('hide');
-	    	});
-	    }
+        if (options && options.previousModels) {
+            _.each(options.previousModels, function(model) {
+                model.trigger('hide');
+            });
+        }
         if (this.collection) {
-            var $list = [];
-            this.collection.forEach(function(item) {
-                var modelView = new this.ModelView({model: item});
-                $list.push(modelView.render().el);
-            }, this);
-            this.$el.html($list);
+            var nodelist = this.collection.reduce(function(nodelist, item) {
+                return nodelist.concat(this.renderItem(item));
+            }, [], this);
+            this.$el.html(this.reverse ? nodelist.reverse() : nodelist);
         }
     },
     render: function() {
@@ -134,14 +165,15 @@ MeiweiApp.PageView = MeiweiApp.View.extend({
             return;
         }
         this.views = {};
-        _.bindAll(this, 'showPage', 'go', 'refresh', 'onResume', 'render', 'reset', 
+        _.bindAll(this, 'showPage', 'go', 'leave', 'refresh', 'onResume', 'render', 'reset', 
                         'onClickLeftBtn', 'onClickRightBtn');
         var $el = this.$el;
         this.$('.wrapper').on('webkitAnimationEnd', function(e) {
-            if (e.originalEvent.animationName == "slideouttoleft") {
-            	$el.trigger('pageClose');
-            } else if (e.originalEvent.animationName == "slideinfromright") {
-            	$el.trigger('pageOpen');
+            var animationName = e.originalEvent.animationName;
+            if (animationName == "slideouttoleft" || animationName == "slideouttoright") {
+                $el.trigger('pageClose');
+            } else if (animationName == "slideinfromright" || animationName == "slideinfromleft") {
+                $el.trigger('pageOpen');
             }
         });
         if (this.initPage) this.initPage();
@@ -171,6 +203,7 @@ MeiweiApp.PageView = MeiweiApp.View.extend({
         });
         page.listenTo(collection, 'reset', page.resetNavigator);
     },
+    leave: function() {},
     go: function(options) {
         this.options = options || {};
         this.reset();
@@ -195,7 +228,8 @@ MeiweiApp.PageView = MeiweiApp.View.extend({
     },
     onResume: function() {},
     reset: function() {},
-    showPage: function() {
+    showPage: function(options) {
+        var options = options || {};
         if (this.$el && this.$el.hasClass('view-hidden')) {
             var $curPage = $('.view:not(".view-hidden")');
             var curPageCloseTimeout;
@@ -206,6 +240,7 @@ MeiweiApp.PageView = MeiweiApp.View.extend({
             };
             $curPage.addClass('view-hidden');
             $curPage.addClass('view-prev');
+            if (options.reverse) $curPage.addClass('view-prev-reverse');
             curPageCloseTimeout = setTimeout(closeCurPage, 1000);
             $curPage.one('pageClose', closeCurPage);
             
@@ -213,13 +248,14 @@ MeiweiApp.PageView = MeiweiApp.View.extend({
             var nextPageOpenTimeout;
             var openNextPage = function() {
                 clearTimeout(nextPageOpenTimeout);
-                $nextPage.removeClass('view-next');
+                $nextPage.removeClass('view-next').removeClass('view-next-reverse');
                 $nextPage.find('input').blur();
                 MeiweiApp.sendGaPageView($nextPage.attr('id')); // Google Analytics
                 window.scrollTo(0, 0);
             };
             $nextPage.removeClass('view-hidden');
             $nextPage.addClass('view-next');
+            if (options.reverse) $nextPage.addClass('view-next-reverse');
             nextPageOpenTimeout = setTimeout(openNextPage, 1000);
             $nextPage.one('pageOpen', openNextPage);
         }
